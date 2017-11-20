a5192ba6437e4a5c7ea174ff9ca2c44eef60773b
Lenin <lenin@pathao.com>
2017-11-20 17:33:41 +0600

    solved closure problem

--- closure/main.go
+++ closure/main.go
@@ -1,16 +1,18 @@
 package main
 
-import "sync"
+import (
+	"sync"
+)
 
 func factory(vals []int, multiplier int) []int {
 	result := make([]int, len(vals))
 	var wg sync.WaitGroup
 	for i, v := range vals {
 		wg.Add(1)
-		go func() {
+		go func(i, v, multiplier int) {
 			defer wg.Done()
 			result[i] = v * multiplier
-		}()
+		}(i, v, multiplier)
 	}
 	wg.Wait()
 	return result
